75b8be08d441edf66f513c339558e9c6c4ba72c1
Dave Laffan <dave.laffan@outlook.com>
2019-05-26 15:48:26 +0000

    Create add_task.html template and set up form

--- app.py
+++ app.py
@@ -15,10 +15,16 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/get_tasks')
 def get_tasks():
-    return render_template("tasks.html", tasks=mongo.db.tasks.find())
-    
+    return render_template("tasks.html",
+    tasks=mongo.db.tasks.find())
+
+@app.route('/add_task')
+def add_task():
+    return render_template('addtask.html',
+    categories=mongo.db.categories.find())
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-    port=int(os.environ.get('PORT')),
-    debug=True)
+        port=int(os.environ.get('PORT')),
+        debug=True)
     
